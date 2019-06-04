bef2e30c577b6cdb748b9fe3ced40f524f6dd29b
João Gabriel Lopes <jglopes6@gmail.com>
2019-06-03 23:18:20 -0300

    Implementada arcball correta

--- main.cpp
+++ main.cpp
@@ -23,6 +23,7 @@ double xRot = 0;
 double yRot = 0;
 double zRot = 0;
 
+double rotation[16] = {1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1};
 
 void drawRect(double x, double y, double z) {
     x /= 2;
@@ -99,28 +100,21 @@ void myDisplay(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
-
     //glScalef(0.5, 0.5, 0.5);
 
     // glPushMatrix();
     // glTranslatef(-0.75, 0, 0);
-    // glRotatef(rot, 1, 1, 0);
-    // drawRect(1, 1, 1);
+    // glRotatef(rot,     rot += 0.01;1, 1, 0);
+    // drawRect(1, 1,     rotate = false;1);
     // glPopMatrix();
     
-    glPushMatrix();
+    //glPushMatrix();
     //glTranslatef(0.75, 0, 0);
-
-    // if (rotate == true)
-    //     glRotatef(angle * 180 / 3.14, axis_in_camera_coord[0], axis_in_camera_coord[1], axis_in_camera_coord[2]);
-    glRotatef(xRot, 1, 0, 0);
-    glRotatef(yRot, 0, 1, 0);
-    glRotatef(zRot, 0, 0, 1);
-    cout << xRot << " " << yRot << " " << zRot << endl;
-
-
+    glLoadMatrixd(rotation);
+    glRotatef(angle * 180 / 3.14, axis_in_camera_coord[0], axis_in_camera_coord[1], axis_in_camera_coord[2]);
+    glGetDoublev(GL_MODELVIEW_MATRIX, rotation);
     drawRect(1, 1, 1);
-    glPopMatrix();
+    //glPopMatrix();
 
     glutSwapBuffers();
 }
@@ -143,9 +137,7 @@ void myMotion(int x, int y) {
 }
 
 glm::vec3 get_arcball_vector(int x, int y) {
-    glm::vec3 P = glm::vec3(1.0*x/screen_width*2 - 1.0,
-                1.0*y/screen_height*2 - 1.0,
-                0);
+    glm::vec3 P = glm::vec3(1.0*x/screen_width*2 - 1.0, 1.0*y/screen_height*2 - 1.0, 0);
     P.y = -P.y;
 
     float OP_squared = P.x * P.x + P.y * P.y;
@@ -154,8 +146,6 @@ glm::vec3 get_arcball_vector(int x, int y) {
         P.z = sqrt(1*1 - OP_squared);  // Pythagoras
     else
         P = glm::normalize(P);  // nearest point
-    
-    //cout << P[0] << " " << P[1] << " " << P[2] << endl;
     return P;
 }
 
@@ -168,25 +158,13 @@ void myReshape(int width, int height) {
 void myIdle() {
     rot += 0.01;
     rotate = false;
+    angle = 0;
     if (cur_mx != last_mx || cur_my != last_my) {
         glm::vec3 va = get_arcball_vector(last_mx, last_my);
         glm::vec3 vb = get_arcball_vector(cur_mx,  cur_my);
 
-        // cout << "VA:" << va[0] << " " << va[1] << " " << va[2] << endl;
-        // cout << "VB:" << vb[0] << " " << vb[1] << " " << vb[2] << endl;
-        // cout << endl;
-
-
         angle = acos(min(1.0f, glm::dot(va, vb)));
         axis_in_camera_coord = glm::cross(va, vb);
-
-        xRot += angle * axis_in_camera_coord[0] * 18000 / 3.14;
-        yRot += angle * axis_in_camera_coord[1] * 18000 / 3.14;
-        zRot += angle * axis_in_camera_coord[2] * 18000 / 3.14;
-        //glRotatef(angle, axis_in_camera_coord[0], axis_in_camera_coord[1], axis_in_camera_coord[2]);
-        //glm::mat3 camera2object = glm::inverse(glm::mat3(transforms[MODE_CAMERA]) * glm::mat3(mesh.object2world));
-        //glm::vec3 axis_in_object_coord = camera2object * axis_in_camera_coord;
-        //mesh.object2world = glm::rotate(mesh.object2world, glm::degrees(angle), axis_in_object_coord);
 
         last_mx = cur_mx;
         last_my = cur_my;
